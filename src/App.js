8569d7d64edab699959deb933a470ff77fbbc8cf
6dean <ferdjallahrockdean@gmail.com>
2022-11-05 21:09:41 +0100

    update css

--- src/App.js
+++ src/App.js
@@ -3,24 +3,36 @@ import "./App.css";
 // import Counter from "./components/Counter";
 
 function App() {
-  const [numbers, setNumbers] = useState([0]);
-
+  const minNumbers = [0];
+  const midNumbers = [0, 0];
+  const maxNumbers = [0, 0, 0];
+  const [numbers, setNumbers] = useState([]);
   //
   return (
     <div className="counter-website">
+      <header>React counter V2</header>
+      <p>Counter available</p>
       <div>
         <button
-          className="new-counter"
+          className={
+            numbers.length === maxNumbers.length ? "h1" : "new-counter"
+          }
           onClick={() => {
             const newNumbers = [...numbers];
             newNumbers.push(0);
             setNumbers(newNumbers);
+            console.log(numbers, newNumbers);
           }}
         >
-          new counter
+          <div>
+            {numbers.length === 0 && <h1>3 / 3</h1>}
+            {numbers.length === minNumbers.length && <h1>2 / 3</h1>}
+            {numbers.length === midNumbers.length && <h1>1 / 3</h1>}
+            {numbers.length === maxNumbers.length && <h1>0 / 3</h1>}
+          </div>
         </button>
       </div>
-      <div className="binding-counters">
+      <div className="binding">
         {numbers.map((elem, index) => {
           if (index < 3) {
             return (
@@ -28,24 +40,26 @@ function App() {
                 <div className="counters">
                   <div className="counter">
                     <button
+                      className="sus-add-button"
                       onClick={() => {
                         const newNumbers = [...numbers];
                         newNumbers[index]--;
                         setNumbers(newNumbers);
                       }}
                     >
-                      -
+                      <div className="sizing"> -</div>
                     </button>
 
                     <div className="result">{elem}</div>
                     <button
+                      className="sus-add-button"
                       onClick={() => {
                         const newNumbers = [...numbers];
                         newNumbers[index]++;
                         setNumbers(newNumbers);
                       }}
                     >
-                      +
+                      <div className="sizing"> +</div>
                     </button>
                   </div>
                   <div className="reset-button">
@@ -66,10 +80,12 @@ function App() {
               </div>
             );
           } else {
+            <h1>0 / 3</h1>;
             alert`Max counter deployed`;
           }
         })}
       </div>
+      <footer>Made by dean at Le Reacteur - November 2022</footer>
     </div>
   );
 }
